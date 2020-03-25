e593975c42baa662330c7c0982de36b93763ccfe
mutska <mutska@ciencias.unam.mx>
2020-03-24 23:44:36 -0600

    Parser arreglado y avance de interp :v

--- Practica3/interp.rkt
+++ Practica3/interp.rkt
@@ -1,14 +1,35 @@
 #lang plai
 (require (file "./parser.rkt"))
+(require (file "./grammars.rkt"))
 
 ;; Recibe una expresión (expr) del lenguaje WAE,
 ;; un id (sub-id) y otra expresión (value).
 ;; Sustituye el valor sub-id por value, en expr.
 ;; subst: WAE symbol WAE -> WAE
-(define (subst expr sub-id value) ...)
+;;(define (subst expr sub-id value) ...)
 
 
 ;; Toma un árbol de sintáxis abstraca del lenguaje WAE
 ;; y lo interpreta, devolviendo el valor numérico correspondiente
 ;; interp: WAE -> number
-(define (interp expr) ...)
+(define (interp expr)
+  (type-case WAE expr
+    [id (i) (error 'interp "Identificador libre >:")]
+    [num (n) n]
+    [op (f l)
+        (cond
+          [(equal? + f) (foldr + 0 (map (λ (expr) (interp expr)) l))]
+          ;;[(equal? - f) (foldl - 0 (map (λ (expr) (interp expr)) l))]
+          [(equal? * f) (foldl * 1 (map (λ (expr) (interp expr)) l))]
+          ;;[(equal? / f) (foldr / 0 (map (λ (expr) (interp expr)) l))]
+          [(equal? modulo f) (modulo (interp (first l)) (interp (second l)))]
+          [(equal? expt f) (expt (interp (first l)) (interp (second l)))]
+          [(equal? add1 f) (add1 (interp (first l)))]
+          [(equal? sub1 f) (sub1 (interp (first l)))]
+          )]
+    [with (bind body) 2]
+    [with* (bind body) 3]
+    ))
+;;(foldr + 0 (map (λ (expr) (interp expr)) l))
+;;Ejemplo (foldr + 0 (map (λ (expr) (interp expr)) (list (num 1) (num 2) (num 3))))
+;;Ehemplo (interp (op + (list (num 1) (num 2) (num 3))))
--- Practica3/parser.rkt
+++ Practica3/parser.rkt
@@ -19,7 +19,7 @@
        [(*) (op * (for/list ((i (cdr sexp))) (parse i)))]
        [(/) (op / (for/list ((i (cdr sexp))) (parse i)))]
        [(modulo) (op modulo (list (parse (second sexp)) (parse (third sexp))))]
-       [(expt) (op expt (list (parse (second sexp)))) ]
+       [(expt) (op expt (list (parse (second sexp)) (parse (third sexp))))]
        [(add1) (op add1 (list (parse (second sexp)))) ]
        [(sub1) (op sub1 (list (parse (second sexp)))) ]
        [(with) (with (for/list ((i (second sexp))) (binding (first i) (parse (second i)))) (parse (third sexp)))]

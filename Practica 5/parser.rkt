fd6719a5175141f0180c1cf4b5c1944488b731a2
mutska <mutska@ciencias.unam.mx>
2020-06-16 18:22:18 -0500

    uff men

--- Practica 5/parser.rkt
+++ Practica 5/parser.rkt
@@ -1,6 +1,6 @@
 #lang plai
 (require (file "./grammars.rkt"))
-
+;;(equal? (condS (list (condition (opS < (list (numS 2) (numS 3))) (numS 1)) (condition (opS >(list (numS 10) (numS 2))) (numS 2)) (else-cond (numS 3)))) (parse '{cond {{< 2 3} 1} {{> 10 2} 2} {else 3}}))
 ;; Toma una lista de números, symbolos o listas
 ;; y la traduce a un árbol de sintaxis abstracta CFWBAE
 ;; A::=<number>
@@ -20,10 +20,13 @@
        [(-) (opS - (for/list ((i (cdr sexp))) (parse i)))]
        [(*) (opS * (for/list ((i (cdr sexp))) (parse i)))]
        [(/) (opS / (for/list ((i (cdr sexp))) (parse i)))]
+       [(>) (opS > (for/list ((i (cdr sexp))) (parse i)))]
+       [(<) (opS < (for/list ((i (cdr sexp))) (parse i)))]
        [(modulo) (opS modulo (list (parse (second sexp)) (parse (third sexp))))]
        [(expt) (opS expt (list (parse (second sexp)) (parse (third sexp))))]
        [(add1) (opS add1 (list (parse (second sexp)))) ]
        [(sub1) (opS sub1 (list (parse (second sexp)))) ]
+       [(cond) (condS (for/list ((i (cdr sexp))) (if (equal? (car i) 'else) (else-cond (parse (car (cdr i)))) (condition (parse (car i)) (parse (cdr (car i))) )) ))]
        [(withS) (withS (for/list ((i (second sexp))) (binding (first i) (parse (second i)))) (parse (third sexp)))]
        [(withS*) (withS* (for/list ((i (second sexp))) (binding (first i) (parse (second i)))) (parse (third sexp)))]
        [(funS) (funS (car (cdr sexp)) (parse (car (cdr (cdr sexp)))))] 

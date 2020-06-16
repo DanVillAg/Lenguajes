996fb59ba9b14a1586ff3ae328a1c9e8aa25c498
mutska <mutska@ciencias.unam.mx>
2020-06-16 18:45:55 -0500

    :v

--- Practica 5/desugar.rkt
+++ Practica 5/desugar.rkt
@@ -9,4 +9,17 @@
 ;; sintáxis abstracta correspondiente a la expresión recibida.
 ;; desugar SCFWBAE-> CFWBAE
 ;; (define (desugar sexpr))
-(define (desugar sexpr) ...)
+(define (desugar sexpr)
+  (type-case SCFWBAE sexpr
+    [idS (i) (id i)]
+    [numS (n) (num n)]
+    [boolS (b) (bool b)]
+    [iFS (condition then else) (iF condition then else)]
+    [opS (f l) (op f l)]
+    [condS (cases) #t]
+    [withS (bind body) #t]
+    [withS* (bind body) #t]
+    [funS (params body) #t]
+    [appS (fun args) #t]     
+    )
+  )
--- Practica 5/parser.rkt
+++ Practica 5/parser.rkt
@@ -1,6 +1,6 @@
 #lang plai
 (require (file "./grammars.rkt"))
-;;(equal? (condS (list (condition (opS < (list (numS 2) (numS 3))) (numS 1)) (condition (opS >(list (numS 10) (numS 2))) (numS 2)) (else-cond (numS 3)))) (parse '{cond {{< 2 3} 1} {{> 10 2} 2} {else 3}}))
+;;
 ;; Toma una lista de números, symbolos o listas
 ;; y la traduce a un árbol de sintaxis abstracta CFWBAE
 ;; A::=<number>
@@ -15,7 +15,7 @@
     [(boolean? sexp) (boolS sexp)]
     [(list? sexp)
      (case (car sexp)
-       [(iFS) (iFS (parse (car (cdr sexp))) (parse (car (cdr (cdr sexp)))) (parse (car (cdr (cdr (cdr sexp))))))]
+       [(if) (iFS (parse (car (cdr sexp))) (parse (car (cdr (cdr sexp)))) (parse (car (cdr (cdr (cdr sexp))))))]
        [(+) (opS + (for/list ((i (cdr sexp))) (parse i)))]
        [(-) (opS - (for/list ((i (cdr sexp))) (parse i)))]
        [(*) (opS * (for/list ((i (cdr sexp))) (parse i)))]
@@ -26,10 +26,11 @@
        [(expt) (opS expt (list (parse (second sexp)) (parse (third sexp))))]
        [(add1) (opS add1 (list (parse (second sexp)))) ]
        [(sub1) (opS sub1 (list (parse (second sexp)))) ]
-       [(cond) (condS (for/list ((i (cdr sexp))) (if (equal? (car i) 'else) (else-cond (parse (car (cdr i)))) (condition (parse (car i)) (parse (cdr (car i))) )) ))]
-       [(withS) (withS (for/list ((i (second sexp))) (binding (first i) (parse (second i)))) (parse (third sexp)))]
-       [(withS*) (withS* (for/list ((i (second sexp))) (binding (first i) (parse (second i)))) (parse (third sexp)))]
-       [(funS) (funS (car (cdr sexp)) (parse (car (cdr (cdr sexp)))))] 
+       [(not)  (opS not  (list (parse (second sexp)))) ]
+       [(cond) (condS (for/list ((i (cdr sexp))) (if (equal? (car i) 'else) (else-cond (parse (car (cdr i)))) (condition (parse (car i)) (parse (car (cdr  i)))  )) ))]
+       [(with) (withS (for/list ((i (second sexp))) (binding (first i) (parse (second i)))) (parse (third sexp)))]
+       [(with*) (withS* (for/list ((i (second sexp))) (binding (first i) (parse (second i)))) (parse (third sexp)))]
+       [(fun) (funS (car (cdr sexp)) (parse (car (cdr (cdr sexp)))))] 
        [else (list? (car sexp)) (appS (funS (car (cdr (car sexp))) (parse (car (cdr (cdr (car sexp)))))) (for/list ((i (car (cdr sexp)))) (parse i)))]
        )]
 

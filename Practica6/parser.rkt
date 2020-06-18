0fdfea02fca17df8e6d551b25d67ce2761aeb4a4
mutska <mutska@ciencias.unam.mx>
2020-06-18 16:10:39 -0500

    >:v

--- Practica6/parser.rkt
+++ Practica6/parser.rkt
@@ -33,13 +33,22 @@
        [(sub1) (opS sub1 (list (parse (second sexp)))) ]
        [(not)  (opS not  (list (parse (second sexp)))) ]
        [(cond) (condS (for/list ((i (cdr sexp))) (if (equal? (car i) 'else) (else-cond (parse (car (cdr i)))) (condition (parse (car i)) (parse (car (cdr  i)))  )) ))]
-       [(with) (withS (for/list ((i (second sexp))) (binding (first i) (parse (second i)))) (parse (third sexp)))]
-       [(with*) (withS* (for/list ((i (second sexp))) (binding (first i) (parse (second i)))) (parse (third sexp)))]
-       [(fun) (funS (car (cdr sexp)) (parse (car (cdr (cdr sexp)))))] 
-       [else (list? (car sexp)) (appS (funS (car (cdr (car sexp))) (parse (car (cdr (cdr (car sexp)))))) (for/list ((i (car (cdr sexp)))) (parse i)))]
+       [(with) (withS (for/list ((i (second sexp))) (binding (first i) (if (equal? (third i) 'number) (numberT) (booleanT)) (parse (fourth i)))) (parse (third sexp)))]
+       [(with*) (withS* (for/list ((i (second sexp))) (binding (first i) (if (equal? (third i) 'number) (numberT) (booleanT)) (parse (fourth i)))) (parse (third sexp)))]
+       [(fun) (funS (aux-param (car (cdr sexp))) (aux-type (cdr sexp)) (parse (fourth (cdr sexp))))] 
+       [else (list? (car sexp)) (appS (parse (car sexp)) (for/list ((i (car (cdr sexp)))) (parse i)))]
        )]
 
   ))
+;;(parse '{{fun {{x : number 3} {y : boolean #f}} : number {if y x 0}} {2 #t}})
+;;(parse '{fun {{x : number 3} {y : number 2}} : number {if y x 0}})
+;;(parse '{with {{x : number 2} {y : boolean #t} {z : number 1}} {if y x z}})
+(define (aux-type s)
+  (if (equal? (third s) 'number) (numberT) (booleanT))
+  )
+(define (aux-param s)
+  (for/list ((i s)) (if (equal? (third i) 'number) (param (first i) (numberT)) (param (first i) (booleanT))))
+  )
 
 (define (aux-or l b)
    (if (empty? l) b (if (boolean? (car l)) (aux-or (cdr l) (or (car l) b)) (aux-or (cdr (car l)) b))))

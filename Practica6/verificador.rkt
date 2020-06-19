ba66e48fee68d1f9d5f4f2611004afa30731eb97
DanVillAg <danielvillegasaguilar@gmial.com>
2020-06-18 19:11:54 -0500

    ADD ya funciona verificación fun

--- Practica6/verificador.rkt
+++ Practica6/verificador.rkt
@@ -8,48 +8,79 @@
 ;; (define (typeof expr context)
 (define (typeof expr context)
   (type-case SCFWBAE expr
-    [idS (i) 1]
+    [idS (i) (contextLookup i context)]
     [numS (n) (numberT)]
     [boolS (b) (booleanT)]
-    [iFS (condition then else) (if (equal? (typeof condition (phi)) (booleanT)) (if (equal? (typeof then (phi)) (typeof else (phi)))  (typeof then (phi)) (error "typeof: Type error\nconditionals must have same type in then-expr and else-expr") ) (error "if: Type error\nConditional's type must be a boolean\nGiven: (numberT)" (typeof condition (phi)) ))]
+    [iFS (condition then else) (if (equal? (typeof condition context) (booleanT)) (if (equal? (typeof then context) (typeof else context))  (typeof then context) (error "typeof: Type error\nconditionals must have same type in then-expr and else-expr") ) (error "if: Type error\nConditional's type must be a boolean\nGiven: (numberT)" (typeof condition context) ))]
     [opS (f l)
          (cond 
-          [(equal? + f) (if (all (for/list ((i l)) (numberT? (typeof i (phi) )))) (numberT) (error "typeof: Error in parameter (boolS #f)\n Expected type: (numberT)\nGiven type: (booleanT)"))]
-          [(equal? - f) (if (all (for/list ((i l)) (numberT? (typeof i (phi) )))) (numberT) (error "typeof: Error in parameter (boolS #f)\n Expected type: (numberT)\nGiven type: (booleanT)"))]
-          [(equal? * f) (if (all (for/list ((i l)) (numberT? (typeof i (phi) )))) (numberT) (error "typeof: Error in parameter (boolS #f)\n Expected type: (numberT)\nGiven type: (booleanT)"))]
-          [(equal? / f) (if (all (for/list ((i l)) (numberT? (typeof i (phi) )))) (numberT) (error "typeof: Error in parameter (boolS #f)\n Expected type: (numberT)\nGiven type: (booleanT)"))]
-          [(equal? < f) (if (all (for/list ((i l)) (numberT? (typeof i (phi) )))) (booleanT) (error "typeof: Error in parameter (numS 1)\nExpected type: (booleanT)\nGiven type: (numberT)"))]
-          [(equal? > f) (if (all (for/list ((i l)) (numberT? (typeof i (phi) )))) (booleanT) (error "typeof: Error in parameter (numS 1)\nExpected type: (booleanT)\nGiven type: (numberT)"))]
-          [(equal? <= f) (if (all (for/list ((i l)) (numberT? (typeof i (phi) )))) (booleanT) (error "typeof: Error in parameter (numS 1)\nExpected type: (booleanT)\nGiven type: (numberT)"))]
-          [(equal? >= f) (if (all (for/list ((i l)) (numberT? (typeof i (phi) )))) (booleanT) (error "typeof: Error in parameter (numS 1)\nExpected type: (booleanT)\nGiven type: (numberT)"))]
-          [(equal? not f) (if (all (for/list ((i l)) (booleanT? (typeof i (phi) )))) (booleanT) (error ""))]
+          [(equal? + f) (if (all (for/list ((i l)) (numberT? (typeof i context )))) (numberT) (error "typeof: Error in parameter (boolS #f)\n Expected type: (numberT)\nGiven type: (booleanT)"))]
+          [(equal? - f) (if (all (for/list ((i l)) (numberT? (typeof i context )))) (numberT) (error "typeof: Error in parameter (boolS #f)\n Expected type: (numberT)\nGiven type: (booleanT)"))]
+          [(equal? * f) (if (all (for/list ((i l)) (numberT? (typeof i context )))) (numberT) (error "typeof: Error in parameter (boolS #f)\n Expected type: (numberT)\nGiven type: (booleanT)"))]
+          [(equal? / f) (if (all (for/list ((i l)) (numberT? (typeof i context )))) (numberT) (error "typeof: Error in parameter (boolS #f)\n Expected type: (numberT)\nGiven type: (booleanT)"))]
+          [(equal? < f) (if (all (for/list ((i l)) (numberT? (typeof i context )))) (booleanT) (error "typeof: Error in parameter (numS 1)\nExpected type: (booleanT)\nGiven type: (numberT)"))]
+          [(equal? > f) (if (all (for/list ((i l)) (numberT? (typeof i context )))) (booleanT) (error "typeof: Error in parameter (numS 1)\nExpected type: (booleanT)\nGiven type: (numberT)"))]
+          [(equal? <= f) (if (all (for/list ((i l)) (numberT? (typeof i context )))) (booleanT) (error "typeof: Error in parameter (numS 1)\nExpected type: (booleanT)\nGiven type: (numberT)"))]
+          [(equal? >= f) (if (all (for/list ((i l)) (numberT? (typeof i context )))) (booleanT) (error "typeof: Error in parameter (numS 1)\nExpected type: (booleanT)\nGiven type: (numberT)"))]
+          [(equal? not f) (if (all (for/list ((i l)) (booleanT? (typeof i context )))) (booleanT) (error ""))]
          )]
-    [condS  (cases) (if (toType cases #t) (getType cases) (error "if: Type error\nConditional's type must be a boolean\nGiven: (numberT)"))]
+    [condS  (cases) (if (toType cases #t context) (getType cases context) (error "if: Type error\nConditional's type must be a boolean\nGiven: (numberT)"))]
     [withS  (bindings body) 1]
     [withS*  (bindings body) 1]
-    [funS (params type body) (funT (append (for/list ((i params)) (param-tipo i)) (list type)))]
+    [funS (params type body)
+          (let ([next-context (addContext params context)])
+          (if (equal? type (typeof body next-context)) ((funT (append (for/list ((i params)) (param-tipo i)) (list type)))) (error 'typeof (string-append "Error in funT type\nExpected type:" (~v(type)) "\nGiven type:" (~v(typeof body next-context)) )) ))] ;; (funT (append (for/list ((i params)) (param-tipo i)) (list type)))]
     [appS (foo args) 1]
     ))
+
+;; como hacer comentarios para errores, según lo visto en la grabación del laboratorio:
+;; (error 'typeof (string-append "Error in funT type\nExpected type:" (~v(funcion-que-devuelde-el-tipo)) "\nGiven type:" (~v(funcion-que-devuelde-el-tipo)) ))
+
+(define (addContext params existing-context)
+  (let ([c (car params)])
+  (if (empty? params) existing-context (addContext (cdr params) (gamma (param-param c) (param-tipo c) existing-context)) )
+  ))
+
+
+;;CON ESTE LOOKUP TODAS LAS FUNCIONES QUE REVISAN EL TYPEOF LLEVAN EL CONTEXTO GLOBAL, ESO HACE AL VERIFICADOR DE EVALUACIÓN DINÁMICA
+;; Busca el identificador "name" en el contexto 
+;;  "context" regresando el tipo correspondiente
+;; o informando un error si no lo encuentra.
+;; lookup: symbol Type-Context -> Type
+(define (contextLookup name context)
+  (type-case Type-Context context
+    [phi() (error' contextLookup "lookup: Hay un identificador libre: x")]
+    [gamma (bound-name bound-type rest-context)
+          (if (symbol=? bound-name name)
+              bound-type
+              (contextLookup name rest-context))]))
+
+#|
+(define-type Type-Context
+  [phi]
+  [gamma (id symbol?)  (tipo Type?) (rest Type-Context?)])
+|#
+
 
 ;;(prueba '{fun {{x : number 3} {y : boolean #f}} : number {if y x 0}})
 ;;(parse'{fun {{x : number 3} {y : boolean #f}} : number {if y x 0}})
 
-(define (toType cases b)
+(define (toType cases b context)
   (let ([c (car cases)]) 
-  (if (condition? c) (if  (equal? (typeof (condition-test-expr c) (phi)) (booleanT)) (toType (cdr cases) #t)  (error "if: Type error\nConditional's type must be a boolean\nGiven: (numberT)"))
+  (if (condition? c) (if  (equal? (typeof (condition-test-expr c) context) (booleanT)) (toType (cdr cases) #t)  (error "if: Type error\nConditional's type must be a boolean\nGiven: (numberT)"))
   b)))
 
-(define (getType cases)
+(define (getType cases context)
   (let ([c (car cases)])
-    (checkConditions (cdr cases) (typeof (condition-then-expr c) (phi)))
+    (checkConditions (cdr cases) (typeof (condition-then-expr c) context) context)
     )
   )
 
-(define (checkConditions cases type)
+(define (checkConditions cases type context)
   (let ([c (car cases)])
     (cond
-      [(condition? c) (if  (equal? (typeof (condition-then-expr c) (phi)) type) (checkConditions (cdr cases) type)  (error "typeof: Type error\nconditionals must have same type in then-expr and else-expr"))]
-      [(else-cond? c) (if  (equal? (typeof (else-cond-else-expr c) (phi)) type) type  (error "typeof: Type error\nconditionals must have same type in then-expr and else-expr"))]
+      [(condition? c) (if  (equal? (typeof (condition-then-expr c) context) type) (checkConditions (cdr cases) type)  (error "typeof: Type error\nconditionals must have same type in then-expr and else-expr"))]
+      [(else-cond? c) (if  (equal? (typeof (else-cond-else-expr c) context) type) type  (error "typeof: Type error\nconditionals must have same type in then-expr and else-expr"))]
       )
 ))
 
